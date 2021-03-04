a481792b6489174b2a2175ae3158fd095aaa9914
diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-04 14:02:32 -0300

    slide finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Slide from './Slide';
 import './App.css';
+import foto1 from './img/1.jpg';
+import foto2 from './img/2.jpg';
+import foto3 from './img/3.jpg';
 
 function App() {
   const slides = [
-    { id: 1, content: 'Slide 1' },
-    { id: 2, content: 'Slide 2' },
-    { id: 3, content: 'Slide 3' },
+    { id: 1, url: foto1 },
+    { id: 2, url: foto2 },
+    { id: 3, url: foto3 },
   ];
 
   return <Slide slides={slides} />;
--- src/Slide.js
+++ src/Slide.js
@@ -1,19 +1,33 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './Slide.module.css';
 
 const Slide = ({ slides }) => {
   const [active, setActive] = useState(0);
+  const [position, setPosition] = useState(0);
+  const contentRef = useRef();
 
-  function slideNext() {}
+  useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(active * width));
+  }, [active]);
 
-  function slidePrev() {}
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
 
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div
+        className={styles.content}
+        style={{ transform: `translateX(${position}px)` }}
+      >
         {slides.map((slide) => (
-          <div className={styles.item} key={slide.id}>
-            {slide.content}
+          <div ref={contentRef} className={styles.item} key={slide.id}>
+            <img src={slide.url} alt="Marvel Hero" />
           </div>
         ))}
       </div>
